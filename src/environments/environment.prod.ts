c3e27ebc729a6300581bc3836af97bd9f28908c3
Dread <x56dragon@gmail.com>
2024-04-14 13:32:57 -0400

    feat(firebase):Integracion inicial con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAVlLgeIMwuZRg8Z9m-mSLgdzYAf2O9xW8",
+    authDomain: "petpalooza-e50e0.firebaseapp.com",
+    projectId: "petpalooza-e50e0",
+    storageBucket: "petpalooza-e50e0.appspot.com",
+    messagingSenderId: "341355020747",
+    appId: "1:341355020747:web:4147b09871022cd0d1b64e",
+    measurementId: "G-87L2E1YE75"
+  }
 };
