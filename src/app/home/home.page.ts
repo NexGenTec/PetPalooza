993cc808db5101de41d7343040cebd97cde26210
NexGenTec <nexgentechnologies2024@gmail.com>
2024-04-27 20:48:50 -0400

    feat(firebase):Se ingresa data con firebase

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { ImgModalPage } from '../img-modal/img-modal.page';
@@ -7,13 +7,15 @@ import { SearchModalPage } from '../search-modal/search-modal.page';
 import * as infoGato from '../../assets/data/InfoGato.json';
 import { Storage } from '@ionic/storage';
 import { WelcomeModalPage } from '../welcome-modal/welcome-modal.page';
+import { FirebaseDatabaseService } from '../service/firebase-database.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class homePage {
+export class homePage implements OnInit {
   infoGato: any = (infoGato as any).default;
   infoPerro: any = (infoPerro as any).default;
   infoPerroChunks: any[][] = [];
@@ -23,7 +25,8 @@ export class homePage {
 
 
   constructor(private router: Router, private modalController: ModalController,
-    private storage: Storage
+    private storage: Storage,
+    private firebaseService: FirebaseDatabaseService
   ) {
     for (let i = 0; i < this.infoPerro.length; i += 1) {
       this.infoPerroChunks.push(this.infoPerro.slice(i, i + 1));
@@ -31,10 +34,39 @@ export class homePage {
     for (let i = 0; i < this.infoGato.length; i += 1) {
       this.infoGatoChunks.push(this.infoGato.slice(i, i + 1));
     }
-    this.combineAnimals();
+    // this.combineAnimals();
     this.showImage = false;
     this.initStorage();
   }
+
+  ngOnInit() {
+    this.cargarDatos();
+  }
+
+  cargarDatos() {
+    this.firebaseService.getGatos().subscribe(gatos => {
+      this.infoGato = gatos;
+      this.infoGatoChunks = this.chunkArray(this.infoGato, 3); // Ejemplo de dividirlos en chunks de 3 elementos
+    });
+
+    this.firebaseService.getPerros().subscribe(perros => {
+      this.infoPerro = perros;
+      this.infoPerroChunks = this.chunkArray(this.infoPerro, 3); // Ejemplo de dividirlos en chunks de 3 elementos
+    });
+  }
+
+
+  chunkArray(array: any[], size: number): any[][] {
+    return array.reduce((chunks, item, index) => {
+      if (index % size === 0) {
+        chunks.push([item]);
+      } else {
+        chunks[chunks.length - 1].push(item);
+      }
+      return chunks;
+    }, []);
+  }
+
 
   navigateToTargetPage(segment: string, gatoId: number) {
     this.router.navigate([segment, gatoId]);
@@ -59,18 +91,18 @@ export class homePage {
     navigation: true
   };
 
-  combineAnimals() {
-    const maxLength = Math.max(this.infoGato.length, this.infoPerro.length);
-    for (let i = 0; i < maxLength; i++) {
-      if (this.infoGato[i]) {
-        this.combinedAnimals.push(this.infoGato[i]);
-      }
-      if (this.infoPerro[i]) {
-        this.combinedAnimals.push(this.infoPerro[i]);
-      }
-    }
+  // combineAnimals() {
+  //   const maxLength = Math.max(this.infoGato.length, this.infoPerro.length);
+  //   for (let i = 0; i < maxLength; i++) {
+  //     if (this.infoGato[i]) {
+  //       this.combinedAnimals.push(this.infoGato[i]);
+  //     }
+  //     if (this.infoPerro[i]) {
+  //       this.combinedAnimals.push(this.infoPerro[i]);
+  //     }
+  //   }
 
-  }
+  // }
   async openSearchModal() {
     const modal = await this.modalController.create({
       component: SearchModalPage,
