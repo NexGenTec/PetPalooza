950604ee1f852d61aac38b1ebdec6530a776fd7b
Nicolas Cabrera <nicolascabreramillacan@gmail.com>
2024-06-03 13:23:59 -0400

    add adoptameDesign

--- src/app/adoptame/adoptame.page.ts
+++ src/app/adoptame/adoptame.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { QuirkyFacts } from '../interface/QuirkyFacts.models';
+import { FirestoreService } from '../service/firestore.service';
+import { Router } from '@angular/router';
+import { ModalController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-adoptame',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdoptamePage implements OnInit {
 
-  constructor() { }
+  DatosFreak: QuirkyFacts[] = [];
+  currentDatoIndex: number = 0;
 
-  ngOnInit() {
+  constructor(private firestores: FirestoreService,
+    private router: Router,
+    private toastController: ToastController
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.getQuirkyFacts();
+    setInterval(() => {
+      this.showRandomQuirkyFact();
+    }, 10000);
+  }
+
+  getQuirkyFacts() {
+    this.firestores.getCollectionChanges<QuirkyFacts>('QuirkyFacts').subscribe(dato => {
+      if (dato) {
+        this.DatosFreak = dato;
+        this.showRandomQuirkyFact();
+      }
+    });
+  }
+
+  showRandomQuirkyFact() {
+    const gatoIndices = this.DatosFreak.map((fact, index) => {
+      return fact.categoria === 'gato' ? index : null;
+    }).filter(index => index !== null);
+
+    if (gatoIndices.length > 0) {
+      const randomIndex = gatoIndices[Math.floor(Math.random() * gatoIndices.length)];
+      this.currentDatoIndex = randomIndex;
+    } else {
+      console.log("No hay datos disponibles con la categoría Gato");
+    }
   }
 
 }
