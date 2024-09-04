8cd237c7284cee751f4241350934838b686be909
Fabian Salas <fabian.salas.astete@gmail.com>
2024-09-04 01:45:52 -0400

    feat: add design img matches

--- src/app/animaciones/confetti.service.ts
+++ src/app/animaciones/confetti.service.ts
@@ -6,20 +6,29 @@ import confetti from 'canvas-confetti';
 })
 export class ConfettiService {
 
+  
   private heartConfetti() {
+    let scalar = 2;
+    let hearts = confetti.shapeFromText({ text: '❤️', scalar });
+    let pets = confetti.shapeFromText({ text: '🐾', scalar });
+
     confetti({
       particleCount: 100,
       angle: 60,
       spread: 55,
-      origin: { x: 0, y: 0 },
-      shapes: ['heart'],
+      origin: { x: 0, y: 1 },
+      // shapes: ['heart'],
+      shapes: [pets],
+      scalar
     });
     confetti({
       particleCount: 100,
       angle: 120,
       spread: 55,
-      origin: { x: 1, y: 0 },
-      shapes: ['heart'],
+      origin: { x: 1, y: 1 },
+      // shapes: ['heart'],
+      shapes: [hearts],
+      scalar
     });
   }
 
