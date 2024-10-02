cbe1a2a7ce2f6000fcc3bdeaed5a2016647348a4
Dread <x56dragon@gmail.com>
2024-10-02 11:44:58 -0300

    hotfix(mapa):Se agrega un marker diferente y segun su categoria

--- src/app/maps/maps.page.ts
+++ src/app/maps/maps.page.ts
@@ -30,6 +30,7 @@ export class MapsPage implements AfterViewInit, OnInit {
   skeletonMap = Array(2);
   presentingElement = null;
   fabPosition: { vertical: string; horizontal: string };
+  userPosition: { lat: number, lng: number } | null = null;
 
   constructor(
     private modalController: ModalController,
@@ -43,6 +44,7 @@ export class MapsPage implements AfterViewInit, OnInit {
     this.setFabPosition();
     this.onFabClick();
     this.permisionLocation();
+    this.checkPermissionAndLocation();
   }
 
   async ionViewWillEnter() {
@@ -294,6 +296,7 @@ export class MapsPage implements AfterViewInit, OnInit {
     if (!this.newMap) {
       return;
     }
+  
     for (const vet of veterinarias) {
       if (vet.Latitud && vet.Longitud) {
         const lat = parseFloat(vet.Latitud);
@@ -301,10 +304,48 @@ export class MapsPage implements AfterViewInit, OnInit {
   
         if (!isNaN(lat) && !isNaN(lng)) {
           try {
+            let svgColor;
+            switch (vet.Categoria) {
+              case 'Veterinarias Movil':
+                svgColor = '#1D4ED8'; // Azul
+                break;
+              case 'Clinicas':
+                svgColor = '#F59E0B'; // Amarillo
+                break;
+              case 'PetShops':
+                svgColor = '#10B981'; // Verde
+                break;
+              case 'Peluquerias':
+                svgColor = '#EF4444'; // Rojo
+                break;
+              case 'Veterinarias':
+              default:
+                svgColor = '#60A5FA'; // Azul claro
+                break;
+            }
+  
+            // Definir el SVG con el color específico
+            const svgMarker = `
+              <svg xmlns:x="http://ns.adobe.com/Extensibility/1.0/" xmlns:i="http://ns.adobe.com/AdobeIllustrator/10.0/" xmlns:graph="http://ns.adobe.com/Graphs/1.0/" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 125" style="enable-background:new 0 0 100 100;" xml:space="preserve" width="40" height="50">
+                <g i:extraneous="self">
+                  <g>
+                    <path fill="${svgColor}" d="M48.4,2.5C31.8,3.3,18.2,17.2,17.7,33.9c-0.2,8,2.5,15.3,7.1,21.1c6.8,8.5,12,18.1,15.8,28.3c1.3,3.5,2.5,6.9,3.6,10.1c1.9,5.5,9.7,5.5,11.6,0c1.1-3.2,2.3-6.6,3.6-10.1c3.7-10.1,8.7-19.7,15.5-28c4.6-5.6,7.4-12.7,7.4-20.5C82.3,16.4,67,1.7,48.4,2.5zM50,57.1c-12.3,0-22.3-10-22.3-22.3s10-22.3,22.3-22.3c12.3,0,22.3,10,22.3,22.3S62.3,57.1,50,57.1z"/>
+                    <path fill="${svgColor}" d="M46.3,28.8c1.9-0.3,3-2.6,2.6-5c-0.4-2.4-2.3-4.1-4.2-3.7c-1.9,0.3-3,2.6-2.6,5C42.6,27.4,44.5,29.1,46.3,28.8z"/>
+                    <path fill="${svgColor}" d="M41.4,35.9c1.7-0.6,2.4-2.8,1.7-5c-0.7-2.2-2.7-3.4-4.4-2.9c-1.7,0.6-2.4,2.8-1.7,5C37.7,35.2,39.7,36.5,41.4,35.9z"/>
+                    <path fill="${svgColor}" d="M53.6,28.8c1.9,0.3,3.7-1.3,4.2-3.7c0.4-2.4-0.7-4.7-2.6-5c-1.9-0.3-3.7,1.3-4.2,3.7C50.6,26.2,51.7,28.4,53.6,28.8z"/>
+                    <path fill="${svgColor}" d="M61.3,28.1c-1.7-0.6-3.6,0.7-4.4,2.9c-0.7,2.2,0,4.4,1.7,5c1.7,0.6,3.6-0.7,4.4-2.9C63.8,30.9,63,28.6,61.3,28.1z"/>
+                  </g>
+                </g>
+              </svg>
+            `;
+  
+            const base64Svg = `data:image/svg+xml;base64,${btoa(svgMarker)}`;
+  
             const vetsId = await this.newMap.addMarker({
               coordinate: { lat, lng },
               title: vet.Nombre,
               snippet: `Dirección: ${vet.Direccion}\nDescripción: ${vet.Descripcion}`,
+              iconUrl: base64Svg,
             });
             this.placeMarkers[vetsId] = vet;
           } catch (error) {
@@ -317,7 +358,7 @@ export class MapsPage implements AfterViewInit, OnInit {
         console.warn(`Localización no definida para la veterinaria: ${vet.Nombre}`, vet);
       }
     }
-  }
+  }    
 
   setMarkerClickListener() {
     this.newMap.setOnMarkerClickListener(async (data) => {
@@ -409,5 +450,22 @@ export class MapsPage implements AfterViewInit, OnInit {
   formatTimestampToTime(timestamp: Timestamp): string {
     const date = timestamp.toDate();
     return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  }
+
+  async checkPermissionAndLocation() {
+    this.userPosition = await this.locationPermitsService.getCurrentPosition();
+  }
+
+  truncateText(text: string, limit: number): string {
+    if (!text) {
+      return '';
+    }
+    const words = text.split(' ');
+    
+    if (words.length > limit) {
+      return words.slice(0, limit).join(' ') + '...';
+    } else {
+      return text;
+    }
   }
 }
