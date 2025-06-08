ef32bf5bb88026c46b453d2c05ab0c3f9008c319
Fabian Salas <fabian.salas.astete@gmail.com>
2025-06-08 13:00:30 -0400

    fix slice to 8

--- src/app/pages/trivia-breed/trivia-breed.page.ts
+++ src/app/pages/trivia-breed/trivia-breed.page.ts
@@ -63,7 +63,7 @@ export class TriviaBreedPage implements OnInit {
           if (imgQuestions && imgQuestions.length > 0) {
 
             const shuffled = this.randomArray([...imgQuestions]);
-            const selected = shuffled.slice(0, 2);
+            const selected = shuffled.slice(0, 8);
 
             this.questionsTrivia = selected;
             this.currentQuestionIndex = 0;
