9dbdfbc96331febde82f5650c8bfc4532ae9d9bd
nexgen <nexgentechnologies2024@gmail.com>
2026-04-28 03:36:28 -0400

    PetPalooza(2025):Version 4.3.5

--- src/app/asistente/asistente.page.ts
+++ src/app/asistente/asistente.page.ts
@@ -9,12 +9,43 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class AsistentePage implements OnInit {
 
   chatUrl: SafeResourceUrl;
+  isLoading: boolean = true;
 
   constructor(private sanitizer: DomSanitizer) {
-    this.chatUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
-      'https://vetassistantai.com/embed/cmobnrjkl0003qr08dmzhwoq2'
-    );
+    const chatUrlWithSpanish = 'https://vetassistantai.com/embed/cmobnrjkl0003qr08dmzhwoq2?' +
+      'pet-care-language=es&' +
+      'language=es&' +
+      'locale=es-ES&' +
+      'i18nextLng=es&' +
+      'preferredLanguage=es&' +
+      'defaultLanguage=es&' +
+      'setLocalStorage=true';
+    
+    this.chatUrl = this.sanitizer.bypassSecurityTrustResourceUrl(chatUrlWithSpanish);
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (typeof document !== 'undefined') {
+      document.documentElement.lang = 'es';
+    }
+    
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 10000);
+  }
+
+  onIframeLoad() {
+    this.isLoading = false;
+    
+    try {
+      const iframe = document.querySelector('.chat-frame') as HTMLIFrameElement;
+      if (iframe) {
+        iframe.style.pointerEvents = 'auto';
+        iframe.style.touchAction = 'auto';
+        
+      }
+    } catch (error) {
+    }
+  }
+
 }
